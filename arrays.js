c19a26408916e699586a68f705e117fd5d270c93
Ozzie Harb <ozharb@gmail.com>
2021-02-10 18:41:05 -0800

    drills 1-8

--- arrays.js
+++ arrays.js
@@ -56,11 +56,79 @@ function main(){
    
     let arr = new Array();
 
-    arr.push("T");
+    arr.push(7);
  
     console.log(mem.get(arr.ptr));
 
 }
-main()
 
+// URLify a string
 
+function URLify(str){
+    str = [...str]
+    for(let i = 0; i < str.length-1; i++){
+        if (str[i]===' '){
+            str[i]='%20'
+        }
+    }
+    return str.join('')
+}
+
+// Filtering an array
+ 
+function lessThanFiveOnly(arr){
+let result = []
+    for(let i = 0; i<arr.length;i++){
+        if(arr[i]<5){
+        result.push(arr[i])
+        }
+    }
+    return result
+}
+
+//7. Max sum in the array
+
+function sum(arr){
+    return arr.reduce((sum,num)=>{
+       return sum+=num
+     }, 0)
+   }
+   
+   function maxSum(arr){
+   if (arr.length==2){
+     sum(arr)
+   } 
+   else {
+   let highestSum = 0
+   let highestSeq
+   for(let i = 0; i < arr.length; i++){
+   let oneArr = arr.slice(0,i+1)
+   if (sum(oneArr) > highestSum){
+     highestSum = sum(oneArr)
+     highestSeq = oneArr
+   }
+   }
+   return {highSum : highestSum,
+           highSeq: highestSeq}
+   }
+   }
+//    8. Merge arrays
+
+   function mergeNsort(arr1,arr2){
+    return arr1.concat(arr2).sort((a,b)=>a-b)
+    }
+
+    function mergeSorted(a,b){
+        a = [...a]
+        
+        for(let i = 0; i < b.length; i ++){
+          for(let j = 0; j< a.length; j++){
+            if(a[j]>b[i]){
+             a.splice(j,0,b[i])
+            break
+            }
+          }
+        }
+        return a
+        }
+        
